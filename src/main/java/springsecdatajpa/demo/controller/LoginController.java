bb0cd2d293ce33ae70c71bef1ae8786261c580db
TomIsLearningJava <tom.ganemo1987@gmail.com>
2023-01-17 15:51:16 +0100

    implemented functions for login using Bcrypt. Created AppuserDto and Converte Classes.

--- src/main/java/springsecdatajpa/demo/controller/LoginController.java
+++ src/main/java/springsecdatajpa/demo/controller/LoginController.java
@@ -1,7 +1,9 @@
 package springsecdatajpa.demo.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import springsecdatajpa.demo.entity.AppUser;
+import springsecdatajpa.demo.entity.DTO.AppUserDTO;
 import springsecdatajpa.demo.entity.DTO.AppUserLoginDTO;
 import springsecdatajpa.demo.service.AppUserService;
 import springsecdatajpa.demo.service.LoginService;
@@ -13,27 +15,18 @@ import springsecdatajpa.demo.util.MapperClass;
 public class LoginController {
 
 
-    private final TokenService tokenService;
-    private final AppUserService appUserService;
     private final LoginService loginService;
-    private MapperClass mapperClass = new MapperClass();
 
-    public LoginController(TokenService tokenService, AppUserService appUserService, LoginService loginService) {
-        this.tokenService = tokenService;
-        this.appUserService = appUserService;
+
+    public LoginController(LoginService loginService) {
         this.loginService = loginService;
     }
 
     @PostMapping("")
-    public String loginScreen(@RequestBody AppUserLoginDTO appUserLoginDTO) {
-        AppUser appUser = appUserService.checkIfValidUser(appUserLoginDTO);
-        if(appUser!=null){
-            String token = tokenService.generateToken(appUser);
-            System.out.println("We have a good user:: "+ appUser);;
-            return token;
-        }else {
-            return "User and user password not found";
-        }
+    public ResponseEntity<AppUserDTO> loginScreen(@RequestBody AppUserLoginDTO appUserLoginDTO) {
+        AppUserDTO appUserDTO = loginService.loginToDatabase(appUserLoginDTO);
+        return ResponseEntity.ok().body(appUserDTO);
     }
+
 
 }
--- src/main/java/springsecdatajpa/demo/service/AppUserService.java
+++ src/main/java/springsecdatajpa/demo/service/AppUserService.java
@@ -15,20 +15,25 @@ public class AppUserService {
         this.appUserRepository = appUserRepository;
     }
 
-    public AppUser checkIfValidUser(AppUserLoginDTO appUserLoginDTO) {
-        System.out.println(appUserLoginDTO.toString());
-        AppUser thisUser = appUserRepository.findAppUserByUserNameAndUserPassword(appUserLoginDTO.getUserName(), appUserLoginDTO.getUserPassword());
+    public AppUser checkIfValidUser(String userName, String userPassword) {
+
+        // TODO: take the Password and hash it --> Compare to the stored hash
+        AppUser thisUser = appUserRepository.findAppUserByUserNameAndUserPassword(userName, userPassword);
         System.out.println(thisUser);
         if (thisUser != null) {
-            System.out.println("not null");
+            System.out.println("User Found in the database and successfully fetched");
             return thisUser;
         } else {
-            System.out.println("is null....");
+            System.out.println("User and Password not found");
             return null;
         }
     }
 
     public void saveAppUser(AppUser u1) {
         appUserRepository.save(u1);
+    }
+
+    public AppUser doesUserExist(String appUserName) {
+        return appUserRepository.findAppUserByUserName(appUserName);
     }
 }
